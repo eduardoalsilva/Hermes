41c9fc7699893913f95d7b259add2f78efb528df
esilva <eduardoalsilva45@gmail.com>
2024-03-07 13:16:02 -0300

    Add reminder_service.py

---
+++ src/reminder_service.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from scheduler import Scheduler
+
+class ReminderService:
+    @staticmethod
+    def schedule_reminder(event_time, reminder_minutes):
+
+        reminder_time = event_time - timedelta(minutes=reminder_minutes)
+        return reminder_time
+    
+    @staticmethod
+    def send_reminder(reminder_info):
+        print(f"Send reminder to '{reminder_info['title']} at {reminder_info['reminder_time']}")
+
+    @staticmethod
+    def process_reminders(scheduler):
+        scheduled_events = Scheduler.get_scheduled_events()
+
+        for event in scheduled_events:
+            if 'reminder' in event:
+                if datetime.now() >= event['reminder_time']:
+                    print(f"Reminder to '{event['title']}' sent!")
+
+                    scheduler.remove_reminder(event['title'])
+
+                else:
+                    print(f"Reminder to '{event['title']} scheduled to {event['reminder_time']}")
