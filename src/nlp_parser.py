9faf6ad5fe9348249fb4eab06ce9cc1a38df3721
esilva <eduardoalsilva45@gmail.com>
2024-03-12 10:04:34 -0300

    Modify nlp_parser.py

--- src/nlp_parser.py
+++ src/nlp_parser.py
@@ -1,9 +1,9 @@
 from dateutil import parser
+import re
 
 class NLPParser:
     @staticmethod
     def parse_command(command):
-
         parsed_info = {
             'title': 'Default event',
             'date': '2024-03-08',
@@ -17,5 +17,9 @@ class NLPParser:
             parsed_info['time'] = parsed_date_time.strftime('%H:%M')
         except ValueError:
             print("Failed to extract date and time information.")
+
+        title_match = re.search(r'"([^"]*)"', command)
+        if title_match:
+            parsed_info['title'] = title_match.group(1)
 
         return parsed_info
